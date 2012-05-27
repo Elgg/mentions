058566c9e71aa6e053f1d3133cf25a2ead39be4e
Emmanuel <emanwebdev@gmail.com>
2012-05-27 18:55:50 +0200

    French language pack

---
+++ languages/fr.php
@@ -0,0 +1,23 @@
+<?php
+
+$french = array(
+	'mentions:send_notification' => 'Envoyer une notification quand quelqu\'un vous mentionne dans un post?',
+	'mentions:notification:subject' => '%s vous a mentionné ici: %s',
+	'mentions:notification:body' => '%s vous a mentionné ici: %s:
+
+"%s"
+
+Pour voir l\'article complet, cliquez sur le lien ci-dessous:
+%s
+',
+	'mentions:notification_types:object:blog' => 'un billet de blog',
+	'mentions:notification_types:object:conversations' => 'une conversation',
+	'mentions:notification_types:annotation:generic_comment' => 'un commentaire',
+	'mentions:notification_types:annotation:wire_reply' => 'une réponse à une conversation',
+
+	'mentions:settings:send_notification' => 'Envoyer une notification quand quelqu\'un vous mentionne dans un post?',
+);
+
+add_translation("fr",$french);
+
+?>
