7e6546f6ec2374cecaa7f30c1a205b1614b65b9b
blackleg <hectorespertpardo@gmail.com>
2017-05-10 09:34:59 +0200

    Basque language translation

---
+++ languages/eu_es.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Basque language translation.
+ */
+
+return array(
+	'mentions:notification:subject' => '%s -an aipatu zaitut %s',
+	'mentions:notification:body' => '%s -an aipatu zaitut %s.
+
+Argitaratze osoa ikusi ahal izateko, klikatu hurrengo estekan:
+%s
+',
+	'mentions:notification_types:object:blog' => 'blogean sarrera bat',
+	'mentions:notification_types:object:bookmarks' => 'markatzailea',
+	'mentions:notification_types:object:groupforumtopic' => 'talde batean eztabaida-mezua',
+	'mentions:notification_types:object:discussion_reply' => 'talde batean eztabaida-erantzuna',
+	'mentions:notification_types:object:thewire' => 'The Wire mezua',
+	'mentions:notification_types:object:comment' => 'iruzkin bat',
+	'mentions:settings:send_notification' => 'Jakinarazpenak jasotzea norbaitek @izena aipatzen zaituenean?',
+
+	// admin
+	'mentions:named_links' => 'Ordezkatu @erabiltzaile erabiltzailearen izenagatik',
+	'mentions:fancy_links' => 'Gehitu irudia erabiltzailearen izenaren ondoan',
+	'mentions:restrict_group_search' => 'Talde baten barruan, iradoki taldearen izenak bakarrik',
+	'mentions:friends_only_search' => 'Iradoki erabiltzailearen lagunak bakarrik',
+
+	'mentions:settings:failed' => 'Ez da aipamenen konfiguraziorik gorde',
+);
