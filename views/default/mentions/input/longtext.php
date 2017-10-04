5c6c7dde3f2e5c6d4c3ced426672bc402c7ed8bf
Olivier Seguin <oseg1@online.fr>
2017-10-04 12:51:26 +0200

    feat(tinymce): add extended_tinymce plugin support

--- views/default/mentions/input/longtext.php
+++ views/default/mentions/input/longtext.php
@@ -3,10 +3,17 @@
 $plugins = [
 	'ckeditor',
 	'tinymce',
+        'extended_tinymce',
 ];
 
+$editor = 'plaintext';
 foreach ($plugins as $plugin) {
 	if (elgg_is_active_plugin($plugin)) {
-		elgg_require_js("mentions/editors/$plugin");
+                $editor = $plugin;
 	}
 }
+
+if($editor == 'extended_tinymce')
+        $editor = 'tinymce';
+
+elgg_require_js("mentions/editors/$editor");
