f5d3210f2b7b5362694ebf4e186da0b3fd8316e1
dragonjon <jonaltuna@msn.com>
2017-10-07 18:14:19 +0200

    Spanish translation

--- languages/es.php
+++ languages/es.php
@@ -1,1 +1,21 @@
+<?php
+/**
+ * This file was created by Translation Editor v6.0
+ * On 2017-10-07 18:11
+ */
 
+return array (
+  'mentions:notification:subject' => '%s te mencionó en %s',
+  'mentions:notification:body' => '%s te mencionó en %s.
+Para ver la publicación completa, pulsa en el siguiente enlace:
+%s',
+  'mentions:notification_types:object:blog' => 'una publicación de blog',
+  'mentions:notification_types:object:bookmarks' => 'un marcador',
+  'mentions:notification_types:object:groupforumtopic' => 'una publicación de un debate de grupo',
+  'mentions:notification_types:object:discussion_reply' => 'una respuesta de un debate de grupo',
+  'mentions:notification_types:object:thewire' => 'una publicación en el hilo',
+  'mentions:notification_types:object:comment' => 'un comentario',
+  'mentions:settings:send_notification' => '¿Enviar una notificación cuando alguien te @temencione a ti en una publicación?',
+  'mentions:fancy_links' => '¿Reemplazar @menciones con una imagen pequeña del usuario en vez de el nombre del usuario?',
+  'mentions:settings:failed' => 'No se puede guardar la configuración de menciones.',
+);
