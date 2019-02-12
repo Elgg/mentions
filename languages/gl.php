e34513e225f11de397fa8bebd33d9e5bb0fe30c9
hectorespert <blackleg@users.noreply.github.com>
2019-02-12 15:16:20 +0100

    chore(i18n): Galician translation

---
+++ languages/gl.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Galician language translation.
+ */
+
+return array(
+	'mentions:notification:subject' => '%s nomeoute en %s',
+	'mentions:notification:body' => '%s nomeoute en %s.
+
+Para ver a publicación completa, fai clic na ligazón seguinte:
+%s
+',
+	'mentions:notification_types:object:blog' => 'unha entrada no blogue',
+	'mentions:notification_types:object:bookmarks' => 'un marcador',
+	'mentions:notification_types:object:groupforumtopic' => 'unha mensaxe de debate nun grupo',
+	'mentions:notification_types:object:discussion_reply' => 'unha resposta de debate nun grupo',
+	'mentions:notification_types:object:thewire' => 'unha mensaxe en The Wire',
+	'mentions:notification_types:object:comment' => 'un comentario',
+	'mentions:settings:send_notification' => 'Recibir notificacións cando alguén te @nomee?',
+
+	// admin
+	'mentions:named_links' => 'Substituír @usuario polo nome do usuario',
+	'mentions:fancy_links' => 'Engadir imaxe xunto ao nome do usuario',
+	'mentions:restrict_group_search' => 'Dentro dun grupo, suxerir só os membros do grupo',
+	'mentions:friends_only_search' => 'Suxerir só os amigos do usuario',
+
+	'mentions:settings:failed' => 'Non se gardou a configuración das mencións.',
+);
